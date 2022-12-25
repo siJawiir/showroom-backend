170787b219737f131685030d83d5999a921c197c
siJawiir <raihanmarwanda@gmail.com>
2022-12-26 05:18:35 +0700

    add customer's views

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Sequelize Script
 
-npx sequelize-cli model:generate --name vehicle --attributes name:string,brand:string,price:integer
+npx sequelize-cli model:generate --name vehicle --attributes name:string,brand:string,price:integer,image:string
 
 npx sequelize-cli model:generate --name customer --attributes name:string,address:string
 
--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -3,25 +3,29 @@ const { customer } = require("../models");
 class CustomerController {
   static async getCustomers(req, res) {
     try {
-      let customers = await customer.findAll();
-
-      res.json(customers);
+      let customers = await customer.findAll({
+        order: [["name", "asc"]],
+      });
+      res.render("customer/index.ejs", { customers });
+      // res.json(customers);
     } catch (err) {
       res.json(err);
     }
   }
-  static createPage(req, res) {}
+  static createPage(req, res) {
+    res.render("customer/addPage.ejs");
+  }
 
   static async create(req, res) {
     try {
-      const { name, adress } = req.body;
+      const { name, address } = req.body;
 
       let result = await customer.create({
         name,
-        adress,
+        address,
       });
-
-      res.json(result);
+      res.redirect("/customers");
+      // res.json(result);
     } catch (err) {
       res.json(err);
     }
@@ -35,37 +39,52 @@ class CustomerController {
         where: { id },
       });
 
-      result === 1
-        ? res.json({ message: `ID ${id} has been deleted` })
-        : res.json({ message: `ID ${id} cannot be deleted` });
+      // result === 1
+      //   ? res.json({ message: `ID ${id} has been deleted` })
+      //   : res.json({ message: `ID ${id} cannot be deleted` });
+
+      res.redirect("/customers");
     } catch (err) {
       res.json(err);
     }
   }
 
-  static async editPage(req, res) {}
+  static async editPage(req, res) {
+    try {
+      {
+        const id = +req.params.id;
+        let result = await customer.findByPk(id);
+
+        res.render("customer/editPage.ejs", { result });
+      }
+    } catch (err) {
+      res.json(err);
+    }
+  }
 
   static async edit(req, res) {
     try {
       const id = +req.params.id;
-      const { name, adress } = req.body;
+      const { name, address } = req.body;
 
       let result = await customer.update(
         {
           name,
-          adress,
+          address,
         },
         {
           where: { id },
         }
       );
-      result[0] === 1
-        ? res.json({
-            message: `ID ${id} has been updated`,
-          })
-        : res.json({
-            message: `ID ${id} cannot updated !`,
-          });
+      // result[0] === 1
+      //   ? res.json({
+      //       message: `ID ${id} has been updated`,
+      //     })
+      //   : res.json({
+      //       message: `ID ${id} cannot updated !`,
+      //     });
+
+      res.redirect("/customers");
     } catch (err) {
       res.json(err);
     }

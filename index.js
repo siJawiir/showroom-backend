ac0b66784e2351ddc9458077d0aa8dceee70cf56
siJawiir <raihanmarwanda@gmail.com>
2022-12-27 05:16:27 +0700

    add API Documentation with Swagger

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const swaggerUI = require('swagger-ui-express')
+const apiDocumentation = require('./apidocs.json')
+
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(apiDocumentation))
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
